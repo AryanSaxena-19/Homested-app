c2d54f9a43c89da9c26fa7a55aaa6608ee227834
aryan <aryansaxena224@gmail.com>
2024-04-11 10:51:23 +0530

    first commit

---
+++ lib/screens/cart/cart_screen.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+import 'package:shop_app/models/Cart.dart';
+import 'package:shop_app/screens/cart/components/check_out_cart.dart';
+
+import 'components/body.dart';
+
+class CartScreen extends StatelessWidget {
+  static String routeName = "/cart";
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: buildAppBar(context),
+      body: Body(),
+      bottomNavigationBar: CheckoutCard(),
+    );
+  }
+
+  AppBar buildAppBar(BuildContext context) {
+    return AppBar(
+      title: Column(
+        children: [
+          Text(
+            "Your Cart",
+            style: TextStyle(color: Colors.black),
+          ),
+          Text(
+            "${demoCarts.length} items",
+            style: Theme.of(context).textTheme.caption,
+          ),
+        ],
+      ),
+    );
+  }
+}
